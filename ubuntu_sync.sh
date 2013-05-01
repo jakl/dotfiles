ceb70b28354dceaf6d298d7fc6a73a87b3b05454
James Koval <james.ross.koval@gmail.com>
2013-05-01 01:14:06 -0700

    fix aircrack; add uprade

--- ubuntu_sync.sh
+++ ubuntu_sync.sh
@@ -6,13 +6,18 @@ function sync_favorite_repos {
 function sync_favorite_apps {
   sudo apt-get install -y aptitude git chromium-browser vim-gnome pavucontrol curl irssi nmap mumble g++ ssh python-software-properties nodejs npm
   sudo npm install -g coffee-script
-
-  #Remove unused ones too
   sudo apt-get purge -y libreoffice-base-core libreoffice-calc libreoffice-common libreoffice-core libreoffice-draw libreoffice-emailmerge libreoffice-gnome libreoffice-gtk libreoffice-help-en-us libreoffice-impress libreoffice-math libreoffice-style-human libreoffice-style-tango libreoffice-writer thunderbird firefox
+  sudo aptitude update -y
+  sudo aptitude full-upgrade -y
+  sudo apt-get auto-remove -y
 }
 
 function install_aircrack {
-  command -v aircrack-ng || sudo dpkg -i <(curl https://launchpad.net/ubuntu/+archive/primary/+files/aircrack-ng_1.1-1.1build1_amd64.deb)
+  if [ ! `command -v aircrack-ng` ]; then
+    wget https://launchpad.net/ubuntu/+archive/primary/+files/aircrack-ng_1.1-1.1build1_amd64.deb
+    sudo dpkg -i aircrack-ng_1.1-1.1build1_amd64.deb
+    rm aircrack-ng_1.1-1.1build1_amd64.deb
+  fi
 }
 
 function main {
