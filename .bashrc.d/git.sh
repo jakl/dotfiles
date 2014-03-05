b151c8a085f5318ff9d04950003eece8018d0d12
James Koval <james.ross.koval@gmail.com>
2014-03-05 15:56:00 -0800

    add aliases for git commit -F - and rebase

--- .bashrc.d/git.sh
+++ .bashrc.d/git.sh
@@ -1,9 +1,10 @@
 alias gs='git status'
 alias ga='git add -A'
 alias gc='git commit'
+alias gcg='git commit -F -'
 alias gf='git fetch'
 alias gd='git diff --color -w'
-alias gdm='gd --diff-filter=M' #modified contents only, not renames or deletes
+alias gdm='gd --diff-filter=M' #diff modified contents only, not renames or deletes
 alias gds='gd --staged'
 alias gl='git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit --date=relative'
 alias glm='gl --author=koval'
@@ -15,3 +16,4 @@ alias gmd='git fetch && git merge origin/deploy'
 alias gmb='git fetch && git merge origin/`git symbolic-ref --short -q HEAD`' #merge a branch named after local branch, from origin
 alias gms='git merge --squash'
 alias gmm='git fetch && git merge origin/master'
+alias grm='git fetch && git rebase origin/master'
