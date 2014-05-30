0356315ea69dfaf7bd122c446b1412f94bf31463
James Koval <james.ross.koval@gmail.com>
2014-05-30 13:47:28 -0700

    search substrings

--- .bashrc.d/functions.sh
+++ .bashrc.d/functions.sh
@@ -52,3 +52,7 @@ whiteboard() {
   # https://gist.github.com/lelandbatey/8677901
   convert "$@" -morphology Convolve DoG:15,100,0 -negate -normalize -blur 0x1 -channel RBG -level 60%,91%,0.1 "whiteboard.$@"
 }
+
+f() {
+  find -E . -iregex ".*$@.*"
+}
