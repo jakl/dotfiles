0823c9b917d755a1779c4fba6ec9cb2f7f7451b3
James Koval <james.ross.koval@gmail.com>
2015-04-20 16:15:58 -0700

    comments

--- .cron.sh
+++ .cron.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash -l
-echo "##### @jakl this is a message I typed at `date` : Starting source ~/.bashrc #####"
+
+# This is an hourly cron script to keep my system well oiled
+
+echo "##### @jakl this is a message I typed on `date` : Starting source ~/.bashrc #####"
 . ~/.bashrc
-echo "##### @jakl this is a message I typed at `date` : Starting brew update... #####"
+echo "##### @jakl this is a message I typed on `date` : Starting brew update... #####"
 /usr/local/bin/brew update
-echo "##### @jakl this is a message I typed at `date` : Starting brew upgrade #####"
+echo "##### @jakl this is a message I typed on `date` : Starting brew upgrade #####"
 /usr/local/bin/brew upgrade
-echo "##### @jakl this is a message I typed at `date` : Starting npm update -g #####"
+echo "##### @jakl this is a message I typed on `date` : Starting npm update -g #####"
 /usr/local/bin/npm update -g
-echo "##### @jakl this is a message I typed at `date` : Starting sync-dottools.sh #####"
+echo "##### @jakl this is a message I typed on `date` : Starting sync-dottools.sh #####"
 /Users/jkoval/.tools-cache/home/aurora/tools/dottools/bin/sync-dottools.sh
-echo "##### @jakl this is a message I typed at `date` : Starting cd ~/workspace/source && git fetch #####"
-cd /Users/jkoval/workspace/source && git fetch
-echo "##### @jakl this is a message I typed at `date` : Done with everything #####"
+echo "##### @jakl this is a message I typed on `date` : Starting cd ~/workspace/source && git fetch #####"
+cd /Users/jkoval/workspace/source && /Users/jkoval/bin/git fetch
+echo "##### @jakl this is a message I typed on `date` : Done with everything #####"
