5d6bf2d7453fdad0c9fa1b17548889ce1f727c7f
James Koval <jkoval@twitter.com>
2015-11-02 12:18:10 -0800

    better error handling in cron

--- .cron.sh
+++ .cron.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash -l
+set -o xtrace -o nounset -o errexit -o pipefail
 
 # This is an hourly cron script to keep my system well oiled
 
-echo "##### @jakl this is a message I typed on `date` : Starting source ~/.bashrc #####"
+echo "##### @jakl cron ran on `date` : Starting source ~/.bashrc #####"
 . ~/.bashrc
-echo "##### @jakl this is a message I typed on `date` : Starting brew update... #####"
+echo "##### @jakl cron ran on `date` : Starting brew update... #####"
 /usr/local/bin/brew update
-echo "##### @jakl this is a message I typed on `date` : Starting brew upgrade #####"
+echo "##### @jakl cron ran on `date` : Starting brew upgrade #####"
 /usr/local/bin/brew upgrade --all
-echo "##### @jakl this is a message I typed on `date` : Starting npm update -g #####"
+echo "##### @jakl cron ran on `date` : Starting npm update -g #####"
 /usr/local/bin/npm update -g
-echo "##### @jakl this is a message I typed on `date` : Starting export SSH_AUTH_SOCK=/tmp/launch-oxZKfu/Listeners && kinit #####"
+echo "##### @jakl cron ran on `date` : Starting export SSH_AUTH_SOCK=/tmp/launch-oxZKfu/Listeners && kinit #####"
 export SSH_AUTH_SOCK=/tmp/launch-oxZKfu/Listeners
-/usr/bin/kinit
-echo "##### @jakl this is a message I typed on `date` : Starting sync-dottools.sh #####"
-/Users/jkoval/.tools-cache/home/aurora/tools/dottools/bin/sync-dottools.sh
-echo "##### @jakl this is a message I typed on `date` : Starting cd ~/workspace/source && git fetch #####"
+/usr/bin/kinit --password-file=/Users/jkoval/.mykinitrc
+echo "##### @jakl cron ran on `date` : Starting cd ~/workspace/source && git fetch #####"
 cd /Users/jkoval/workspace/source && /Users/jkoval/bin/sgit fetch
-echo "##### @jakl this is a message I typed on `date` : Done with everything #####"
+echo "##### @jakl cron ran on `date` : Done with everything #####"
