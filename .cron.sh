38088d0209234cfd3e138b74c185c6d05332b63a
James Koval <jkoval@twitter.com>
2016-01-20 15:59:36 -0700

    remove old things

--- .cron.sh
+++ .cron.sh
@@ -11,9 +11,4 @@ echo "##### @jakl cron ran on `date` : Starting brew upgrade #####"
 /usr/local/bin/brew upgrade --all
 echo "##### @jakl cron ran on `date` : Starting npm update -g #####"
 /usr/local/bin/npm update -g
-echo "##### @jakl cron ran on `date` : Starting export SSH_AUTH_SOCK=/tmp/launch-oxZKfu/Listeners && kinit #####"
-export SSH_AUTH_SOCK=/tmp/launch-oxZKfu/Listeners
-/usr/bin/kinit --password-file=/Users/jkoval/.mykinitrc
-echo "##### @jakl cron ran on `date` : Starting cd ~/workspace/source && git fetch #####"
-cd /Users/jkoval/workspace/source && /Users/jkoval/bin/sgit fetch
 echo "##### @jakl cron ran on `date` : Done with everything #####"
